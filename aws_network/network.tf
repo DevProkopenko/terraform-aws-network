f28d08ae0971bf2c0d70ec55c47de9f0db235941
Prokopenko Artsiom <prokopenko.art@mail.ru>
2022-06-18 20:36:39 +0300

    ver2

--- aws_network/network.tf
+++ aws_network/network.tf
@@ -19,8 +19,8 @@ resource "aws_subnet" "public_subnets" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = element(var.public_subnet_cidrs, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index] #Читает и берёт по очереди
-  map_public_ip_on_launch = true                                                     #Добавление IP public
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
   tags = {
     Name = "${var.env}-public-${count.index + 1}"
   }
