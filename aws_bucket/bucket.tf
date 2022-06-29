299bf2dd44e3eb3cb65dae4136f9fab2d4505b89
Prokopenko Artsiom <prokopenko.art@mail.ru>
2022-06-29 10:00:32 +0300

    test

--- aws_bucket/bucket.tf
+++ aws_bucket/bucket.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "prokopenko-artsiom-terraform-bucket"
+  bucket = var.bucket_name
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.example.id
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_versioning" "example" {
   }
 }
 resource "aws_dynamodb_table" "bucket" {
-  name         = "prokopenko-artsiom-terraform-bucket"
+  name         = var.bucket_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
