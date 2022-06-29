7c7058e15f5b003d8537759541ae2fbb5d1f8aa0
DevProkopenko <105281060+DevProkopenko@users.noreply.github.com>
2022-06-29 12:14:10 +0300

    Update buckettfstate.tf

--- aws_network/buckettfstate.tf
+++ aws_network/buckettfstate.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "prokopenko-artsiom-terraform-bucket"
     key            = "dev/network/terraform.tfstate"
-    region         = "eu-central-1"
+    region         = "eu-west-1"
     dynamodb_table = "prokopenko-artsiom-terraform-bucket"
     encrypt        = true
 
