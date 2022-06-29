1cbcb0dbed1e9894652c687e6ebcdc3a397fc3da
DevProkopenko <105281060+DevProkopenko@users.noreply.github.com>
2022-06-29 10:42:46 +0300

    Update bucket.tf

--- aws_bucket/bucket.tf
+++ aws_bucket/bucket.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name[0]
+  bucket = var.bucket
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = var.bucket_name
+  bucket = var.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -10,13 +10,13 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   }
 }
 resource "aws_s3_bucket_versioning" "example" {
-  bucket = var.bucket_name[0]
+  bucket = var.bucket
   versioning_configuration {
     status = "Enabled"
   }
 }
 resource "aws_dynamodb_table" "bucket" {
-  name         = var.bucket_name[0]
+  name         = var.bucket
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
