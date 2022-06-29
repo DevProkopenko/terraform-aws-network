d6bc39da3d743cbcd4ba23014a55e2305bc66ae6
DevProkopenko <105281060+DevProkopenko@users.noreply.github.com>
2022-06-29 15:48:54 +0300

    Update outputs.tf

--- aws_network/outputs.tf
+++ aws_network/outputs.tf
@@ -1,8 +1,8 @@
 output "vpc_id" {
-  value = aws_default_vpc.default.id
+  value = aws_vpc.main.id
 }
 output "vpc_cidr" {
-  value = aws_default_vpc.default.cidr_block
+  value = aws_vpc.main.cidr_block
 }
 output "public_subnets_ids" {
   value = aws_subnet.public_subnets[*].id
