726c464018560b6798eb7ab3ab1ec070125ea672
DevProkopenko <105281060+DevProkopenko@users.noreply.github.com>
2022-06-29 10:38:20 +0300

    Update bucket.tf

--- aws_bucket/bucket.tf
+++ aws_bucket/bucket.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.example.id
+  bucket = var.bucket_name
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   }
 }
 resource "aws_s3_bucket_versioning" "example" {
-  bucket = aws_s3_bucket.example.id
+  bucket = var.bucket_name
   versioning_configuration {
     status = "Enabled"
   }
